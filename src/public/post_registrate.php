603100afb10d1103b0295a3920e872b067b8a5d3
ElizaF03 <elizafad03@gmail.com>
2024-01-18 18:11:11 +0800

    commit

--- src/public/post_registrate.php
+++ src/public/post_registrate.php
@@ -6,3 +6,5 @@ $password= $_POST['password'];
 
 $pdo= new PDO("pgsql:host=postgres; port=5432; dbname=test_db", username:"elizaveta", password:"qwerty");
 $pdo->exec("INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')");
+
+echo 'success!';
