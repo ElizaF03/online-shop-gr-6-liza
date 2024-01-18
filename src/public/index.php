def44c2a0dccb575b8bbd9ac680effa38347218c
ElizaF03 <elizafad03@gmail.com>
2024-01-18 15:06:46 +0800

    init commit

---
+++ src/public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+//$pdo= new PDO("pgsql:host=postgres; port=5433; dbname=test_db", username:"elizaveta", password:"qwerty");
+//$pdo->exec('CREATE TABLE users (name varchar(255) not null, age integer)');
